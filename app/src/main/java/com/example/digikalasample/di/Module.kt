9d26be787db67cdd87332dc1021c86d47a660a24
Nick Gharizadeh <nickgharizadeh@gmail.com>
2022-06-05 23:33:57 +0430

    fea/add module object for di

---
+++ app/src/main/java/com/example/digikalasample/di/Module.kt
@@ -0,0 +1,46 @@
+package com.example.digikalasample.di
+
+import com.example.digikalasample.data.DigiKalaApiService
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+
+@InstallIn(SingletonComponent::class)
+@Module
+object Module {
+
+    @Singleton
+    @Provides
+    fun getMoshi(): Moshi {
+        val moshi = Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+        return moshi
+    }
+
+    @Singleton
+    @Provides
+    fun getRetrofit(moshi: Moshi): Retrofit {
+        val retrofit = Retrofit.Builder()
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .baseUrl("https://woocommerce.maktabsharif.ir/wp-json/wc/v3/")
+            .build()
+        return retrofit
+    }
+
+
+    @Singleton
+    @Provides
+    fun getApiService(retrofit: Retrofit): DigiKalaApiService {
+        return retrofit.create(DigiKalaApiService::class.java)
+    }
+
+
+}
