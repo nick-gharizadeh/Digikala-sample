d4dec3baa84fa73a7a6608733b505ed4520246fe
Nick Gharizadeh <nickgharizadeh@gmail.com>
2022-06-06 12:58:04 +0430

    fea/add get product function to product remote data source

--- app/src/main/java/com/example/digikalasample/data/ProductRemoteDataSource.kt
+++ app/src/main/java/com/example/digikalasample/data/ProductRemoteDataSource.kt
@@ -1,9 +1,13 @@
 package com.example.digikalasample.data
 
+import com.example.digikalasample.data.model.Product
 import com.example.digikalasample.network.DigiKalaApiService
 import javax.inject.Inject
 
-class ProductRemoteDataSource @Inject constructor(val ApiService : DigiKalaApiService) {
+class ProductRemoteDataSource @Inject constructor(val productApiService : DigiKalaApiService) {
 
+    suspend fun getProducts( orderBy:String ):List<Product>{
+        return productApiService.getProduct(orderBy = orderBy)
+    }
 
 }
