67c5e8f40024af416044984d263e257c3f807557
Nick Gharizadeh <nickgharizadeh@gmail.com>
2022-06-06 12:58:46 +0430

    fea/add get product function to view model

--- app/src/main/java/com/example/digikalasample/viewmodel/ProductViewModel.kt
+++ app/src/main/java/com/example/digikalasample/viewmodel/ProductViewModel.kt
@@ -1,12 +1,27 @@
 package com.example.digikalasample.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.digikalasample.data.model.Product
 import com.example.digikalasample.data.repository.ProductsRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ProductViewModel @Inject constructor(val productRepository: ProductsRepository) :
     ViewModel() {
+    val popularProductList = MutableLiveData<List<Product?>>()
 
+    init {
+        getProducts("popularity")
+    }
+
+    fun getProducts(orderBy: String) {
+        viewModelScope.launch {
+            val list = productRepository.getProducts(orderBy = orderBy)
+            popularProductList.value = list
+        }
+    }
 }
