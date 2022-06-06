82f8c6fa667a830352b225a2400fa96662c7e0ca
Nick Gharizadeh <nickgharizadeh@gmail.com>
2022-06-06 13:00:27 +0430

    fix/add @AndroidEntryPoint to view files

--- app/src/main/java/com/example/digikalasample/ui/MainFragment.kt
+++ app/src/main/java/com/example/digikalasample/ui/MainFragment.kt
@@ -5,11 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.digikalasample.R
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import com.example.digikalasample.databinding.FragmentMainBinding
+import com.example.digikalasample.ui.adapter.ProductAdapter
+import com.example.digikalasample.viewmodel.ProductViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class MainFragment : Fragment() {
-
+    lateinit var binding: FragmentMainBinding
+    val viewModel: ProductViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -18,10 +24,18 @@ class MainFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+    ): View {
+        binding = FragmentMainBinding.inflate(layoutInflater)
+        return binding.root
     }
 
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val adapter = ProductAdapter()
+        binding.mostViewRecyclerView.adapter = adapter
+        viewModel.popularProductList.observe(viewLifecycleOwner) {
+            if (it !=null)
+            adapter.submitList(it)
+        }
+    }
 }
