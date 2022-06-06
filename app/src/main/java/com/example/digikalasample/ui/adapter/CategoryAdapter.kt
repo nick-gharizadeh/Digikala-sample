e2853add3e28c9f4d63c258b0d8bb2eaa94a2eba
Nick Gharizadeh <nickgharizadeh@gmail.com>
2022-06-06 15:31:16 +0430

    fea/add category adapter

---
+++ app/src/main/java/com/example/digikalasample/ui/adapter/CategoryAdapter.kt
@@ -0,0 +1,49 @@
+package com.example.digikalasample.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.digikalasample.R
+import com.example.digikalasample.data.model.Category
+import com.example.digikalasample.databinding.CategoryItemBinding
+
+
+class CategoryAdapter() :
+    ListAdapter<Category, CategoryAdapter.ItemHolder>(MovieDiffCallback) {
+    class ItemHolder(val binding: CategoryItemBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemHolder {
+        val binding:CategoryItemBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            R.layout.category_item,
+            parent,false
+        )
+        return ItemHolder(binding)
+
+    }
+
+    override fun onBindViewHolder(holder: ItemHolder, position: Int) {
+        val category = getItem(position)
+        holder.binding.category = category
+
+
+    }
+}
+
+
+object MovieDiffCallback : DiffUtil.ItemCallback<Category>() {
+
+    override fun areItemsTheSame(oldItem: Category, newItem: Category): Boolean {
+        return oldItem == newItem
+    }
+
+    override fun areContentsTheSame(oldItem: Category, newItem: Category): Boolean {
+        return oldItem.id == newItem.id
+    }
+}
