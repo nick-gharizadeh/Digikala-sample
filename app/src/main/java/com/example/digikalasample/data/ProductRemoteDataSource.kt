9bd56145acd9786f85a440ecfc83708efb1e537e
Nick Gharizadeh <nickgharizadeh@gmail.com>
2022-06-06 15:32:47 +0430

    fea/add function to get categories in product remote data source

--- app/src/main/java/com/example/digikalasample/data/ProductRemoteDataSource.kt
+++ app/src/main/java/com/example/digikalasample/data/ProductRemoteDataSource.kt
@@ -1,13 +1,18 @@
 package com.example.digikalasample.data
 
+import com.example.digikalasample.data.model.Category
 import com.example.digikalasample.data.model.Product
 import com.example.digikalasample.network.DigiKalaApiService
 import javax.inject.Inject
 
-class ProductRemoteDataSource @Inject constructor(val productApiService : DigiKalaApiService) {
+class ProductRemoteDataSource @Inject constructor(val productApiService: DigiKalaApiService) {
 
-    suspend fun getProducts( orderBy:String ):List<Product>{
+    suspend fun getProducts(orderBy: String): List<Product> {
         return productApiService.getProduct(orderBy = orderBy)
+    }
+
+    suspend fun getCategories(): List<Category> {
+        return productApiService.getCategories()
     }
 
 }
