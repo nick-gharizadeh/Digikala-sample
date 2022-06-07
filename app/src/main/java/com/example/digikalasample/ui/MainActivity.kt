3e4187b3444b963644610f3586b1b3a1631a75d4
Nick Gharizadeh <nickgharizadeh@gmail.com>
2022-06-08 00:45:12 +0430

    fix/add splash screen

--- app/src/main/java/com/example/digikalasample/ui/MainActivity.kt
+++ app/src/main/java/com/example/digikalasample/ui/MainActivity.kt
@@ -1,14 +1,29 @@
 package com.example.digikalasample.ui
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.digikalasample.R
+import com.example.digikalasample.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        supportActionBar?.hide()
+        binding.imageView.alpha=0f
+        binding.imageView.animate().setDuration(2000).alpha(1f).withEndAction{
+            binding.imageView.visibility= View.GONE
+            supportActionBar?.show()
+            binding.constraint.setBackgroundColor(Color.TRANSPARENT)
+            binding.fragmentContainerView.visibility= View.VISIBLE
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+        }
+        }
 }
