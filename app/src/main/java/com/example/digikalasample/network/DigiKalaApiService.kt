e933950a5a77a588ac377b5b55e0cd57a6bcb279
Nick Gharizadeh <nickgharizadeh@gmail.com>
2022-06-09 23:12:38 +0430

    imp/ change some naming of variables

--- app/src/main/java/com/example/digikalasample/network/DigiKalaApiService.kt
+++ app/src/main/java/com/example/digikalasample/network/DigiKalaApiService.kt
@@ -8,12 +8,12 @@ import retrofit2.http.Query
 
 const val CONSUMER_KEY = "ck_0e61e4a29d9130ed954bb8c11e7f827a7e8c2dfb"
 const val CONSUMER_SECRET = "cs_46699340821cdba8425c23e67c0af8672e7dac88"
-const val WOOCOMMERCE_REST_AUTHENTICATION_KEY =
+const val AUTHENTICATION_KEY =
     "?consumer_key=$CONSUMER_KEY&consumer_secret=$CONSUMER_SECRET"
 
 interface DigiKalaApiService {
 
-    @GET("products$WOOCOMMERCE_REST_AUTHENTICATION_KEY")
+    @GET("products$AUTHENTICATION_KEY")
     suspend fun getProduct(
         @Query("per_page")
         perPage: Int = 15,
@@ -23,7 +23,7 @@ interface DigiKalaApiService {
         orderBy: String
     ): List<Product>
 
-    @GET("products$WOOCOMMERCE_REST_AUTHENTICATION_KEY")
+    @GET("products$AUTHENTICATION_KEY")
     suspend fun getProduct(
         @Query("category")
         category: Int ,
@@ -33,7 +33,7 @@ interface DigiKalaApiService {
         numberOfPage: Int = 1,
     ): List<Product>
 
-    @GET("products/categories${WOOCOMMERCE_REST_AUTHENTICATION_KEY}&per_page=30")
+    @GET("products/categories${AUTHENTICATION_KEY}&per_page=30")
     suspend fun getCategories(
     ): List<Category>
 
