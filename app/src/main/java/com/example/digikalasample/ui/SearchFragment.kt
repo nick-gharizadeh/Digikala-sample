f0a9065d87a5d6c4a0a841eec113b86af0fae008
Nick Gharizadeh <nickgharizadeh@gmail.com>
2022-06-15 11:47:43 +0430

    fea/show menu as live search

---
+++ app/src/main/java/com/example/digikalasample/ui/SearchFragment.kt
@@ -0,0 +1,55 @@
+package com.example.digikalasample.ui
+
+import android.os.Bundle
+import android.view.*
+import androidx.appcompat.widget.SearchView
+import androidx.fragment.app.Fragment
+import com.example.digikalasample.R
+
+
+class SearchFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        setHasOptionsMenu(true)
+        return inflater.inflate(R.layout.fragment_search, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.search_menu, menu)
+        val searchItem = menu.findItem(R.id.item_menu_search_fragment)
+        val searchView = searchItem.actionView as SearchView
+        searchView.onQueryTextChanged {
+
+        }
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+}
+
+inline fun SearchView.onQueryTextChanged(crossinline listener: (String) -> Unit) {
+    this.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+        override fun onQueryTextSubmit(query: String?): Boolean {
+            return true
+        }
+
+        override fun onQueryTextChange(newText: String?): Boolean {
+            listener(newText.orEmpty())
+            return true
+        }
+
+    })
+}
