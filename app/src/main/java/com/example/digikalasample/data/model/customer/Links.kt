42ba7bd6995002d493e786cb2c629c0b1519d2c3
Nick Gharizadeh <nickgharizadeh@gmail.com>
2022-06-21 11:42:44 +0430

    fea /add customer data class

---
+++ app/src/main/java/com/example/digikalasample/data/model/customer/Links.kt
@@ -0,0 +1,6 @@
+package com.example.digikalasample.data.model.customer
+
+data class Links(
+    val collection: List<Collection>,
+    val self: List<Self>
+)
---
+++ app/src/main/java/com/example/digikalasample/data/model/customer/Self.kt
@@ -0,0 +1,5 @@
+package com.example.digikalasample.data.model.customer
+
+data class Self(
+    val href: String
+)
