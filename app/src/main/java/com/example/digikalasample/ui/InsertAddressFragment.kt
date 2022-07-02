598085b9aa3b0a52ffac5df56d0b40d238a96669
Nick Gharizadeh <nickgharizadeh@gmail.com>
2022-07-02 17:14:54 +0430

    fea /show location on map

--- app/src/main/java/com/example/digikalasample/ui/InsertAddressFragment.kt
+++ app/src/main/java/com/example/digikalasample/ui/InsertAddressFragment.kt
@@ -1,12 +1,27 @@
 package com.example.digikalasample.ui
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.location.LocationManagerCompat
 import androidx.fragment.app.Fragment
 import com.example.digikalasample.R
 import com.example.digikalasample.databinding.FragmentInsertAddressBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority.PRIORITY_HIGH_ACCURACY
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -14,9 +29,11 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
+
 class InsertAddressFragment : Fragment() {
     private lateinit var binding: FragmentInsertAddressBinding
     private lateinit var map: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -30,22 +47,78 @@ class InsertAddressFragment : Fragment() {
         return binding.root
     }
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        getLocationPermission()
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
         val mapFragment = childFragmentManager
             .findFragmentById(R.id.myMap) as SupportMapFragment
         mapFragment.getMapAsync(OnMapReadyCallback { readyMap ->
             map = readyMap
-            showLocationOnMap(LatLng(37.4219983, 122.084))
-
         })
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun getLocationPermission() {
+        val locationPermissionRequest = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            when {
+                permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                    showLocation()
+                }
+                permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                    showLocation()
+                }
+                else -> {
+                    Toast.makeText(requireContext(), "goodbye", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+        locationPermissionRequest.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        )
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun showLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        if (!isLocationEnabled()) {
+            Toast.makeText(requireContext(), "turn on your location", Toast.LENGTH_SHORT).show()
+        }
+
+        fusedLocationClient.getCurrentLocation(PRIORITY_HIGH_ACCURACY, null)
+            .addOnSuccessListener { location: Location? ->
+                location?.let {
+                    showLocationOnMap(LatLng(it.latitude, it.longitude))
+                }
+            }
 
 
     }
 
+    private fun isLocationEnabled(): Boolean {
+        val locationManager =
+            activity?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return LocationManagerCompat.isLocationEnabled(locationManager)
+    }
+
     private fun showLocationOnMap(latLng: LatLng) {
-        map.setMinZoomPreference(3.0f)
-        map.setMaxZoomPreference(14.0f)
+        map.setMinZoomPreference(2.0f)
+        map.setMaxZoomPreference(18.0f)
         map.cameraPosition.zoom
         map.addMarker(
             MarkerOptions()
