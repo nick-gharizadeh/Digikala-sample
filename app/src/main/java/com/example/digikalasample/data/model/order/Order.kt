73754e1b22b879f694b196c57d310989c5e3449f
Nick Gharizadeh <nickgharizadeh@gmail.com>
2022-07-05 21:04:49 +0430

    fea / make design of change theme fragment

--- app/src/main/java/com/example/digikalasample/data/model/order/Order.kt
+++ app/src/main/java/com/example/digikalasample/data/model/order/Order.kt
@@ -6,7 +6,6 @@ data class Order(
     val customer_id: Int,
     val id: Int = 0,
     val line_items: List<LineItem>,
-    val total: String = "",
     val billing: Billing,
     val coupon_lines: List<CouponLine>
 )
