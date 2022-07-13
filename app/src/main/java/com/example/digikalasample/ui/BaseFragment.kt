b1644b77ddaee00e9d52bb20e5d84116214e0322
Nick Gharizadeh <nickgharizadeh@gmail.com>
2022-07-13 13:35:35 +0430

    imp /show dialog once

--- app/src/main/java/com/example/digikalasample/ui/BaseFragment.kt
+++ app/src/main/java/com/example/digikalasample/ui/BaseFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import com.example.digikalasample.R
 import com.example.digikalasample.data.model.statusLiveData
@@ -11,7 +12,7 @@ import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 
 open class BaseFragment : Fragment() {
-
+    var dialog: androidx.appcompat.app.AlertDialog? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,12 +31,20 @@ open class BaseFragment : Fragment() {
 
 
     private fun showDialog(message: String) {
-        MaterialAlertDialogBuilder(requireContext())
+        if (dialog == null) {
+            dialog = buildDialog(message)
+        } else if (!dialog!!.isShowing) {
+            dialog = buildDialog(message)
+        }
+        statusLiveData.value = null
+    }
+
+    private fun buildDialog(message: String): AlertDialog? {
+        return MaterialAlertDialogBuilder(requireContext())
             .setTitle(getString(R.string.error_String))
             .setMessage(message)
             .setPositiveButton(getString(R.string.okay_dialog_button)) { dialog, which ->
             }
             .show()
-        statusLiveData.value = null
     }
 }
